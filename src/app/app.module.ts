3798cf913cd6c75069c754cb88b5b8fc22205b6b
Jared Eklin <jaredeklin@gmail.com>
2018-09-14 14:39:31 -0600

    Create idea-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IdeaFormComponent } from './idea-form/idea-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeaFormComponent
   ],
   imports: [
     BrowserModule
